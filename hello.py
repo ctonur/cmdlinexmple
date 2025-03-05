48fdd13ad913cc1cd74dcab5efd102fcd5f150df
ctonur <35571438+ctonur@users.noreply.github.com>
2025-03-05 19:36:51 +0100

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+import time
+import sys
+
+print("Merhaba Dunya!")
+
+# Komut satiri argumentlerini yazdir.
+print(f"Komut satiri argumentleri: {sys.argv}")
+
+# Bir sure calismaya devam et.
+time.sleep(60)
